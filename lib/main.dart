f25936342537a2716569353841328ba58a08eafe
dd_nssit <izdihar@nssit.com.my>
2025-06-18 18:17:09 +0800

    Testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,73 @@
 // <editor-fold desc="Library">
 import 'package:flutter/material.dart';
-// </editor-fold>
-
-// <editor-fold desc="Main">
-void main() {
-  runApp(const MyApp());
-}
 
 // <editor-fold desc="template">
 // </editor-fold>
 
 // </editor-fold>
 
+// <editor-fold desc="Main">
+void main() {
+  runApp(MyApp());
+}
+// </editor-fold>
+
 // <editor-fold desc="Stateless Widget">
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(title: 'Hello World Demo', home: HelloWorldScreen());
   }
 }
 // </editor-fold>
 
 // <editor-fold desc="Stateful Widget">
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
+class HelloWorldScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HelloWorldScreenState createState() => _HelloWorldScreenState();
 }
-// </editor-fold>
 
-// <editor-fold desc="Homepage">
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HelloWorldScreenState extends State<HelloWorldScreen> {
+  String _message = 'Click the button!';
 
-  void _incrementCounter() {
+  void _sayHello() {
     setState(() {
-      _counter++;
+      _message = 'Hello, World!';
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: Text('Hello Demo')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          children: [
+            Text(_message, style: TextStyle(fontSize: 24)),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: _sayHello, child: Text('Say Hello')),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+// </editor-fold>
+
+// <editor-fold desc="Function">
+
+// <editor-fold desc="Test Function">
+String test_Function(String str_para1, [str_para2 = "default"]) {
+  print("Test function");
+
+  int no1 = 1;
+  no1++;
+
+  print(no1);
+
+  return "Hello $str_para1 and $str_para2";
+}
+// </editor-fold>
+
 // </editor-fold>
